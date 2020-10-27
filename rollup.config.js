f0d6e5014bb884160b4f911b4b4fba35ae57b7b8
jardapernik <jaroslav.pernica9@gmail.com>
2020-10-27 06:50:14 +0100

    add iife min version

--- rollup.config.js
+++ rollup.config.js
@@ -2,12 +2,19 @@ import { terser } from "rollup-plugin-terser";
 
 export default {
 	input: "./src/index.js",
-	output: {
-		file: "./dist/index.js",
-		format: "cjs",
-		name: "pre-frame",
-		exports: "named"
-	},
+	output: [{
+  		file: "./dist/pre-frame.js",
+  		format: "cjs",
+  		name: "preframe",
+  		exports: "named"
+  	},
+    {
+      file: "./dist/pre-frame.min.js",
+      format: "iife",
+      name: "preframe",
+      exports: "named"
+    }
+  ],
   external: ['preact'],
 	plugins: [
 		terser()
